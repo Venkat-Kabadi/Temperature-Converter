b1e10812656463ce01a2c81b5f38ab0f2fe6767d
Venkat Kabadi <167209161+Venkat-Kabadi@users.noreply.github.com>
2024-09-28 22:00:30 +0530

    Add files via upload

---
+++ Temperature Converter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+temp=float(input("Enter the temperature: "))
+unit=input("Is it in Celcius or Fahrenheit (C or F): ")
+if unit=="C":
+    temp=round((9*temp)/5+32,1)
+    print(f"{temp}°F")
+elif unit=="F":
+    temp=round((temp-32)*5/9,1)
+    print(f"{temp}°C")
+else:
+    print(f"{unit} is an invalid unit of measurement")
+
+
+# In[ ]:
+
+
+
+
